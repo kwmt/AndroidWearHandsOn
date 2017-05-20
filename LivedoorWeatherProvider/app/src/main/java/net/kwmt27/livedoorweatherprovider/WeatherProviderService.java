8e0e91037380b027902e6af3608bf9153df02fd5
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-05-20 14:30:11 +0900

    add weatherproviderservice

---
+++ LivedoorWeatherProvider/app/src/main/java/net/kwmt27/livedoorweatherprovider/WeatherProviderService.java
@@ -0,0 +1,10 @@
+package net.kwmt27.livedoorweatherprovider;
+
+import android.app.Service;
+
+/**
+ * Created by kwmt on 2017/05/20.
+ */
+
+public class WeatherProviderService extends Service {
+}
