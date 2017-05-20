e659e22a2e4a4af91c785d63ae5374d570dc999f
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-05-20 15:49:56 +0900

    ファイル作成

---
+++ MyWathFace/app/src/main/java/net/kwmt27/mywathface/ComplicationConfigActivity.java
@@ -0,0 +1,13 @@
+package net.kwmt27.mywathface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ComplicationConfigActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complication_config);
+    }
+}
