b4b5d8c2bf84a65428e95c78404f41877cd8f828
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-05-20 14:09:31 +0900

    add ProviderService

---
+++ LivedoorWeatherProvider/app/src/main/java/net/kwmt27/livedoorweatherprovider/ProviderService.java
@@ -0,0 +1,13 @@
+package net.kwmt27.livedoorweatherprovider;
+
+import android.support.wearable.complications.ComplicationManager;
+import android.support.wearable.complications.ComplicationProviderService;
+
+
+public class ProviderService extends ComplicationProviderService {
+
+    @Override
+    public void onComplicationUpdate(int complicationId, int dataType, ComplicationManager complicationManager) {
+
+    }
+}
