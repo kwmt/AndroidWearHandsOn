e53cc5dd03cb91d18b091f090141864b3f142f0a
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-05-20 14:19:51 +0900

    impl ComplicationProviderService

--- LivedoorWeatherProvider/app/src/main/java/net/kwmt27/livedoorweatherprovider/ProviderService.java
+++ LivedoorWeatherProvider/app/src/main/java/net/kwmt27/livedoorweatherprovider/ProviderService.java
@@ -1,13 +1,19 @@
 package net.kwmt27.livedoorweatherprovider;
 
+import android.support.wearable.complications.ComplicationData;
 import android.support.wearable.complications.ComplicationManager;
 import android.support.wearable.complications.ComplicationProviderService;
+import android.support.wearable.complications.ComplicationText;
 
 
 public class ProviderService extends ComplicationProviderService {
 
     @Override
     public void onComplicationUpdate(int complicationId, int dataType, ComplicationManager complicationManager) {
+        ComplicationData complicationData = new ComplicationData.Builder(dataType)
+                .setShortText(ComplicationText.plainText("test"))
+                .build();
 
+        complicationManager.updateComplicationData(complicationId, complicationData);
     }
 }
